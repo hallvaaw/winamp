2f336cb6c9871668625417ffe80bd1c3db923d4c
hallvaaw <hallvard_wahler@hotmail.com>
2022-12-10 23:07:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <main class="bg-slate-800">
+        <div class="container flex flex-col mx-auto">
+            <section id="top" class="flex flex-col bg-slate-700"></section>
+                <div class="flex flex-row bg-slate-700 justify-between">
+                    <div class="bg-gray-400">&nbsp;</div>
+                    <div class="items-center">
+                        <div class="bg-gray-900 rounded border-[1px] border-gray-800 border-b-gray-400 border-r-gray-400 text-green-500 tracking-widest font-['Verdana'] text-xl py-2 pr-16 pl-1">WELCOME TO MY HOMEPAGE</div>
+                    </div>
+                </div>
+                <div class="flex flex-row justify-center bg-slate-700">
+                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2">^ SHUFFLE</button>
+                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2">^ SHUFFLE</button>
+                </div>
+            <section id="middle" class="bg-green-200">&nbsp;</section>
+            <section id="bottom" class="flex flex-col bg-slate-800">
+                <div class="border-2 border-gray-800 border-b-gray-200 border-r-gray-200 bg-gray-900">
+                    <p class="text-amber-100 tracking-widest font-['Verdana']">1. DJ Mike Llama - Llama Whippin' Intro</p>
+                    <p class="text-green-500 tracking-widest font-['Verdana']">1. DJ Mike Llama - Llama Whippin' Intro</p>
+                    <p class="text-green-500 tracking-widest font-['Verdana']">1. DJ Mike Llama - Llama Whippin' Intro</p>
+                    <p class="text-green-500 tracking-widest font-['Verdana']">1. DJ Mike Llama - Llama Whippin' Intro</p>
+                </div>
+                <div></div>
+            </section>
+            <h1 class="bg-blue-200">Hello world!</h1>
+        </div>
+    </main>
+</template>
+
+<script>
+</script>
