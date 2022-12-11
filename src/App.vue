a7d94338b5a3d25ece13cfd74b8cd63c7dbb5327
hallvaaw <hallvard_wahler@hotmail.com>
2022-12-11 22:43:20 +0100

    updated header and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
     <main class="bg-slate-800">
+        <header class="container flex flex-row mx-auto text-center justify-center">
+            <div class="flex flex-col">
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[296px] -pt-2">&nbsp;</div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px] -pt-2">&nbsp;</div>
+            </div>
+            <h1 class="text-slate-300 text-2xl font-bold tracking-wider font-['Verdana'] pt-2">HALLVARD A. WÆHLER</h1>
+            <div class="flex flex-col">
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[296px]">&nbsp;</div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px]">&nbsp;</div>
+            </div>
+        </header>
         <div class="container flex flex-col mx-auto">
-            <section id="top" class="flex flex-col bg-slate-700"></section>
+            <section id="top" class="flex flex-col bg-slate-700">
                 <div class="flex flex-row bg-slate-700 justify-between">
-                    <div class="bg-gray-400">&nbsp;</div>
+                    <div class="bg-gray-400 p-12">A&nbsp;</div>
                     <div class="items-center">
-                        <div class="bg-gray-900 rounded border-[1px] border-gray-800 border-b-gray-400 border-r-gray-400 text-green-500 tracking-widest font-['Verdana'] text-xl py-2 pr-16 pl-1">WELCOME TO MY HOMEPAGE</div>
+                        <div class="bg-gray-900 border-2 border-gray-600 border-t-gray-900 border-l-gray-900 text-green-500 tracking-widest font-['Verdana'] text-xl py-2 pr-16 pl-1"><marquee>WELCOME TO MY HOMEPAGE</marquee></div>
                     </div>
                 </div>
+                <div class="bg-slate-800 border-2 border-gray-600 border-t-gray-900 border-l-gray-900 mx-12">&nbsp;</div>
                 <div class="flex flex-row justify-center bg-slate-700">
-                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2">^ SHUFFLE</button>
+                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2 hover:border-r-gray-200 hover:border-t-gray-500 hover:border-b-0 hover:border-t-6">^ SHUFFLE</button>
                     <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2">^ SHUFFLE</button>
                 </div>
+            </section>
             <section id="middle" class="bg-green-200">&nbsp;</section>
             <section id="bottom" class="flex flex-col bg-slate-800">
                 <div class="border-2 border-gray-800 border-b-gray-200 border-r-gray-200 bg-gray-900">
@@ -22,7 +35,6 @@
                 </div>
                 <div></div>
             </section>
-            <h1 class="bg-blue-200">Hello world!</h1>
         </div>
     </main>
 </template>
