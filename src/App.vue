fe5362dd2bcb6e4d5a94038c57f57929a86a717d
hallvaaw <hallvard_wahler@hotmail.com>
2022-12-11 23:59:23 +0100

    added sections

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="bg-slate-800">
-        <header class="container flex flex-row mx-auto text-center justify-center">
+        <header class="container flex flex-row border-b-2 border-b-gray-900 mx-auto text-center justify-center">
             <div class="flex flex-col my-4">
                 <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[296px] h-2"></div>
                 <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px] h-2"></div>
@@ -11,21 +11,42 @@
                 <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px] h-2"></div>
             </div>
         </header>
-        <div class="container flex flex-col mx-auto">
+        <div class="container flex flex-col border-t-[1px] border-t-gray-400 mx-auto">
             <section id="top" class="flex flex-col bg-slate-700">
                 <div class="flex flex-row bg-slate-700 justify-between">
-                    <div class="bg-gray-400 p-12">A&nbsp;</div>
+                    <div class="bg-gray-400 h-36 w-56 p-12"></div>
                     <div class="items-center">
                         <div class="bg-gray-900 border-2 border-gray-600 border-t-gray-900 border-l-gray-900 text-green-500 tracking-widest font-['Verdana'] text-xl py-2 pr-16 pl-1"><marquee>WELCOME TO MY HOMEPAGE</marquee></div>
                     </div>
                 </div>
                 <div class="bg-slate-800 border-2 border-gray-600 border-t-gray-900 border-l-gray-900 h-8 mx-12"></div>
-                <div class="flex flex-row justify-center bg-slate-700">
-                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2 hover:border-r-gray-200 hover:border-t-gray-500 hover:border-b-0 hover:border-t-6">^ SHUFFLE</button>
-                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2">^ SHUFFLE</button>
+        <div class="container flex flex-row border-b-2 border-b-gray-900 mx-auto text-center justify-center">
+            <div class="flex flex-col my-4">
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[296px] h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px] h-1.5"></div>
+            </div>
+            <h1 class="text-slate-300 text-2xl font-bold tracking-wider font-['Verdana'] pt-2">ABOUT</h1>
+            <div class="flex flex-col my-4">
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[296px] h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px] h-1.5"></div>
+            </div>
+        </div>
+                <div class="flex flex-row justify-left bg-slate-700">
+                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2 active:border-l-gray-200 active:border-t-gray-500 active:border-b-0 active:border-t-6">GITHUB</button>
+                    <button class="border-4 border-b-gray-500 border-x-gray-600 border-t-gray-200 rounded bg-gray-300 tracking-widest font-['Verdana'] p-1 pr-2 my-2 active:border-r-gray-200 active:border-t-gray-500 active:border-b-0 active:border-t-6">LINKEDIN</button>
                 </div>
             </section>
-            <section id="middle" class="bg-green-200">&nbsp;</section>
+        <div class="container flex flex-row border-b-2 border-b-gray-900 mx-auto text-center justify-center">
+            <div class="flex flex-col my-4">
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[296px] h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px] h-1.5"></div>
+            </div>
+            <h1 class="text-slate-300 text-2xl font-bold tracking-wider font-['Verdana'] pt-2">MORE THINGS</h1>
+            <div class="flex flex-col my-4">
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[296px] h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[296px] h-1.5"></div>
+            </div>
+        </div>
             <section id="bottom" class="flex flex-col bg-slate-800">
                 <div class="border-2 border-gray-800 border-b-gray-200 border-r-gray-200 bg-gray-900">
                     <p class="text-amber-100 tracking-widest font-['Verdana']">1. DJ Mike Llama - Llama Whippin' Intro</p>
