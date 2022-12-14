51486f866d79bb5706052330610dcf57e778599f
hallvaaw <hallvard_wahler@hotmail.com>
2022-12-14 22:33:37 +0100

    added icon frames

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <header class="container flex flex-row mx-auto text-center justify-center px-64">
             <div class="flex flex-col my-4">
                 <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-40 h-2"></div>
-                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-40] h-2"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-40 h-2"></div>
             </div>
             <h1 class="text-slate-300 text-2xl font-bold tracking-wider font-['Verdana'] pt-2">HALLVARD A. WÆHLER</h1>
             <div class="flex flex-col my-4">
@@ -28,28 +28,35 @@
                 <div class="bg-slate-800 border-2 border-gray-600 border-t-gray-900 border-l-gray-900 h-8 mx-16 my-8"></div>
         <div class="container flex flex-row bg-slate-800 border-2 border-x-4 border-gray-600 border-t-gray-900 border-x-slate-600 mx-auto text-center justify-center pb-2">
             <div class="flex flex-col my-4">
-                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-40 h-1.5"></div>
-                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-40 h-1.5"></div>
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-56 h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-56 h-1.5"></div>
             </div>
             <h1 class="text-slate-300 text-2xl font-bold tracking-wider font-['Verdana'] pt-2">ABOUT</h1>
             <div class="flex flex-col my-4">
-                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-40 h-1.5"></div>
-                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-40 h-1.5"></div>
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-56 h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-56 h-1.5"></div>
             </div>
         </div>
                 <div class="flex flex-row justify-left bg-slate-700 mb-6">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p class="text-xl text-gray-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div class="flex flex-row">
+                        <div class="items-center mx-2">
+                            <div class="bg-amber-300 h-16 w-3 border-2 rounded-t-xl border-gray-800 border-b-0 ml-2.5"></div>
+                            <div class="bg-gray-400 h-9 w-9 border-2 rounded-md border-gray-800 border-l-gray-200"></div>
+                            <div class="bg-amber-300 h-16 w-3 border-2 rounded-b-xl border-gray-800 border-t-0 ml-2.5"></div>
+                        </div>
+                    </div>
                 </div>
             </section>
         <div class="container flex flex-row border-y-2 border-x-4 border-x-slate-600 border-b-gray-600 border-t-gray-900 mx-auto text-center justify-center pb-2">
             <div class="flex flex-col my-4">
-                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-48 h-1.5"></div>
-                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-48 h-1.5"></div>
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[198px] h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[198px] h-1.5"></div>
             </div>
             <h1 class="text-slate-300 text-2xl font-bold tracking-wider font-['Verdana'] pt-2">MORE THINGS</h1>
             <div class="flex flex-col my-4">
-                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-48 h-1.5"></div>
-                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-48 h-1.5"></div>
+                <div class="bg-amber-200 border-2 border-b-[1px] rounded-t-xl rounded-b border-gray-900 px-[198px] h-1.5"></div>
+                <div class="bg-amber-300 border-2 border-t-[1px] rounded-b-xl rounded-t border-gray-900 px-[198px] h-1.5"></div>
             </div>
         </div>
             <section id="bottom" class="flex flex-col bg-slate-800 min-h-screen">
